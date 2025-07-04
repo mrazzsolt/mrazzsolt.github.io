fad2c56322c1e80d817c5220f0c9c342e529d007
mrazzsolt <85168335+mrazzsolt@users.noreply.github.com>
2025-07-04 15:41:03 +0200

    improve style and content

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-document.getElementById('theme-toggle').addEventListener('click', () => {
-  document.body.classList.toggle('light-mode');
+const toggleBtn = document.getElementById("theme-toggle");
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
 });
